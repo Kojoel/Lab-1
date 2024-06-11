d26e07b327b899f0dfa5101dad5e594a9400b83f
Kojoel <nanaabruquah1s@gmail.com>
2024-06-11 20:59:59 +0000

    Array transformations - double, filterEven, sum, average

--- FDTP.js
+++ FDTP.js
@@ -1,4 +1,4 @@
-// String transformations
+/////////////////// String transformations
 
 //  a. capitalize(str)
 function capitalize(text) {
@@ -58,3 +58,51 @@ function wordCount(text) {
     console.log(result);
 }
 wordCount("this counts the number of words")
+
+
+
+/////////////////// Array Transformatioms
+
+//  a. double(arr)
+function double(arr= [2, 3, 4, 5, 6, 7, 8, 10, 12]){
+    let result = [];
+    for(let i=0; i<arr.length; i++){
+      result[i]= arr[i]*2;
+    }
+    console.log(result);
+  }
+  double();
+  
+  //  b. filterEven(arr)
+  function filterEven(arr= [2, 3, 4, 5, 6, 7, 8, 10, 12]){
+    let result = [];
+    for(let i=0; i<arr.length; i++){
+      if(arr[i]%2 === 0){
+        result[i] = arr[i];
+      }
+    }
+    console.log(result);
+  }
+  filterEven();
+  
+  //  c.sum(arr)
+  function sum(arr= [2, 3, 4, 5, 6, 7, 8, 10, 12]){
+    let sum_result = 0;
+    for(let i=0; i<arr.length; i++){
+      sum_result += arr[i];
+    }
+    console.log(sum_result);
+  }
+  sum();
+  
+  //  d. average(arr)
+  function average(arr= [2, 3, 4, 5, 6, 7, 8, 10, 12]){
+    let avg = 0;
+    let total= 0;
+    for(let i=0; i<arr.length; i++){
+      total += arr[i];
+    }
+    avg = total/arr.length;
+    console.log(avg);
+  }
+  average();
