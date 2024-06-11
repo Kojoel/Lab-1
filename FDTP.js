317039a214d0b20183fb7d042d9d2c0a57f6df9f
Kojoel <nanaabruquah1s@gmail.com>
2024-06-11 20:58:23 +0000

    String transformation - wordCount

--- FDTP.js
+++ FDTP.js
@@ -18,36 +18,43 @@ function reverse(text) {
     let result = "";
     let counter = text.length - 1;
     if(typeof(text) === "string") {
-      for(let i=0; i<text.length; i++) {
-      result += text[counter];
-      counter--;
-      }
-      console.log(result);
-    }
+        for(let i=0; i<text.length; i++) {
+            result += text[counter];
+            counter--;
+            }
+            console.log(result);
+        }
     else{
       console.log("Invalid input");
     }
-  }
-  reverse("hello");
+}
+reverse("hello");
 
   //  c. isPalindrome
 function isPalindrome(text) {
     let result = "";
     let counter = text.length - 1;
     if(typeof(text) === "string") {
-      for(let i=0; i<text.length; i++) {
-      result += text[counter];
-      counter--;
-      }
-      if(result === text){
+        for(let i=0; i<text.length; i++) {
+        result += text[counter];
+        counter--;
+        }
+        if(result === text){
         console.log("It is a palindrome");
-      }
-      else{
+        }
+        else{
         console.log("It isn't a palindrome")
-      }
+        }
     }
     else{
-      console.log("Invalid input");
+        console.log("Invalid input");
     }
-  }
-  isPalindrome("ama");
+}
+isPalindrome("ama");
+
+// d. wordCount
+function wordCount(text) {
+    let result = text.split(" ").length;
+    console.log(result);
+}
+wordCount("this counts the number of words")
